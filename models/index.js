5b9823566f24a827bb185fe9f471fb64b2ed9deb
Golnaz8 <golnaz.brj@gmail.com>
2023-09-01 17:54:27 -0400

    added models

--- models/index.js
+++ models/index.js
@@ -2,13 +2,27 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-Post.belongsTo(User, {
+
+User.hasMany(Post, {
   foreignKey: 'userId',
   onDelete: 'CASCADE'
 });
 
+Post.belongsTo(User, {
+  foreignKey: 'userId'
+});
+
 Post.hasMany(Comment, {
   foreignKey: 'postId',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'postId'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'userId',
   onDelete: 'CASCADE'
 });
 
